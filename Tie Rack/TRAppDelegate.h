1edd2c27faac11dea36b8d5f42ed4903869f4dd6
Nate Wilson <nate.wilson@gmail.com>
2013-05-21 16:26:09 -0400

    Initial Commit

---
+++ Tie Rack/TRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRAppDelegate.h
+//  Tie Rack
+//
+//  Created by Nate Wilson on 5/21/13.
+//  Copyright (c) 2013 iOS Study Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tie Rack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tie Rack
+//
+//  Created by Nate Wilson on 5/21/13.
+//  Copyright (c) 2013 iOS Study Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRAppDelegate class]));
+    }
+}
