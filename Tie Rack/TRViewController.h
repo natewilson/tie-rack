5eb83c7a70f8d7e809693365375e25bdc8182570
renecandelier <renej.jorge@gmail.com>
2013-05-28 12:31:01 -0400

    Added view and image.

---
+++ Tie Rack/TRViewController.h
@@ -0,0 +1,13 @@
+//
+//  TRViewController.h
+//  Tie Rack
+//
+//  Created by Rene Candelier on 5/28/13.
+//  Copyright (c) 2013 iOS Study Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRViewController : UIViewController
+
+@end
---
+++ Tie Rack/TRViewController.m
@@ -0,0 +1,38 @@
+//
+//  TRViewController.m
+//  Tie Rack
+//
+//  Created by Rene Candelier on 5/28/13.
+//  Copyright (c) 2013 iOS Study Group. All rights reserved.
+//
+
+#import "TRViewController.h"
+
+@interface TRViewController ()
+
+@end
+
+@implementation TRViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
