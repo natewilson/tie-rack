e5b996ba04cce7f1a0f4f2078f1e4db04c02d8c4
Nate Wilson <nate.wilson@gmail.com>
2013-08-26 18:55:18 -0400

    scroll view in use

--- Tie Rack/TRAppDelegate.h
+++ Tie Rack/TRAppDelegate.h
@@ -4,11 +4,8 @@
 //
 //  Created by Nate Wilson on 5/21/13.
 //  Copyright (c) 2013 iOS Study Group. All rights reserved.
-//test2
-// mark test
 
 #import <UIKit/UIKit.h>
-#import "TRViewController.h"
 
 @interface TRAppDelegate : UIResponder <UIApplicationDelegate>
 
