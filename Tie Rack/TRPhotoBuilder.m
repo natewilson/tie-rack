95feea6de56f57bbb0c7ae2136e1b81783c5cbed
Nate Wilson <nate.wilson@gmail.com>
2013-10-18 11:43:58 -0400

    Image construction and tie positioning is complete.  Just have the whole “wrong tie” bug to work through now. -N8

--- Tie Rack/TRPhotoBuilder.m
+++ Tie Rack/TRPhotoBuilder.m
@@ -51,18 +51,25 @@
                 // Try to make a UIImage out of what we have
                 NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:buf];
                 UIImage *originalPhoto = [[UIImage alloc] initWithData:imageData];
-                NSLog(@"Base size %f, %f", originalPhoto.size.width, originalPhoto.size.height);
+                NSLog(@"Original (i.e. camera) size %f, %f", originalPhoto.size.width, originalPhoto.size.height);
                 
-                // NOTE: SCALE UP!! (we have the resolution)
+                // Getting ready to scale up (b/c we have the resolution) start with the size of the preview layer
                 CGSize previewSize = [[ctrl previewLayer] bounds].size;
-                NSLog(@"Preview size %f, %f", previewSize.width, previewSize.height);
-                CGFloat scale = MIN((CGFloat)originalPhoto.size.width / previewSize.width, (CGFloat)originalPhoto.size.height / previewSize.height);
+                NSLog(@"Preview (i.e. screen) size %f, %f", previewSize.width, previewSize.height);
+                
+                // Figure out whether we scale by width or height - selecing the smallest factor to fill the entire area
+                // Note: this assumes the photo from the camera has a greater resolution than the screen on the device
+                CGFloat scale = MIN((CGFloat)originalPhoto.size.width / previewSize.width,
+                                    (CGFloat)originalPhoto.size.height / previewSize.height);
                 NSLog(@"Using a factor of %f and", scale);
+                
+                // Now build an object to hold onto our final size.
                 CGSize finalSize = CGSizeMake(scale * previewSize.width, scale * previewSize.height);
                 NSLog(@"Scaling to %f, %f", finalSize.width, finalSize.height);
         
-                
-                // TODO - INTRODUCE AND OFFSET FOR THE BASE IMAGE.  EVERYTHING ELSE WORKS!!!!
+                // For both width and height check whether we need to offset the area of the photo to use
+                // this helps us grab the actual portion used in the preview layer when showing video gravity that
+                // fills the entire layer.  (Lots of guessing to get here... thanks Apple.)
                 CGPoint offsets;
                 offsets.x = (scale * previewSize.width  < originalPhoto.size.width ) ?
                             (originalPhoto.size.width - scale * previewSize.width) / 2.0 : 0;
@@ -70,63 +77,59 @@
                             (originalPhoto.size.height - scale * previewSize.height) / 2.0 : 0;
                 NSLog(@"Offsetting by %f, %f", offsets.x, offsets.y);
                 
-                // Crop the new image
-                CGRect cropRect = CGRectMake(0+offsets.x, 0+offsets.y, finalSize.width-offsets.x, finalSize.height-offsets.y);
+                // Crop the new image with a rectandle we're going to build
+                CGRect cropRect;
+                // Figure out whether we're cropping straight-ways or sideways (landscape) and build a rectangle with the right offset
+                if (originalPhoto.imageOrientation == UIImageOrientationDown || originalPhoto.imageOrientation == UIImageOrientationUp) {
+                    cropRect = CGRectMake(0+offsets.x, 0+offsets.y, finalSize.width, finalSize.height);
+                } else { // if we're in landscape just swap the x/y and width/height args to account for the change.
+                    NSLog(@"Due to a landscape orientation...");
+                    cropRect = CGRectMake(0+offsets.y, 0+offsets.x, finalSize.height, finalSize.width);
+                }
+                NSLog(@"Cropping area origin is: %f, %f \n\twith size: %f, %f",
+                      cropRect.origin.x, cropRect.origin.y,
+                      cropRect.size.width, cropRect.size.height);
+                
+                // Now slice that baby up just right
                 CGImageRef imageRef = CGImageCreateWithImageInRect([originalPhoto CGImage], cropRect);
-                UIImage *base = [UIImage imageWithCGImage:imageRef];
+                // ...preserving the original orientation
+                UIImage *base = [UIImage imageWithCGImage:imageRef scale:1.0 orientation:originalPhoto.imageOrientation];
+                // ...and cleaning up after ourselves.
                 CGImageRelease(imageRef);
                 
-                // Now scale the translation to the new size and update the completed transformation for the tie.
-                CGPoint scaledTranslation = CGPointMake(translation.x * scale, translation.y * scale);
-                CGAffineTransform totalTransform = CGAffineTransformConcat(ctm,
-                                                                           CGAffineTransformMakeTranslation(scaledTranslation.x,
-                                                                                                            scaledTranslation.y));
+                // Current status
+                // *base is a handle to the scaled up resolution of what the user saw on the screen
                 
                 // Setup an image context and get a reference to it.  This is where we will build the image
                 UIGraphicsBeginImageContext(finalSize);
                 CGContextRef refContext = UIGraphicsGetCurrentContext();
                 
-                // Start by drawing the base image. (Hopefully in the refContext and not tieContextRef)
+                // Now write our base image to the active context
                 [base drawInRect:CGRectMake(0, 0, finalSize.width, finalSize.height)];
+
                 
-                // We'll draw the tie in a separate layer.
-                // Create it first based on the size of the preview layer (so we match the scale the user sees)
-                //CGSize tieLayerSize = [[ctrl previewLayer] bounds].size;
-                //CGLayerRef tieLayer = CGLayerCreateWithContext(refContext, tieLayerSize, nil);
-                //NSLog(@"tieLayerSize: %f, %f", tieLayerSize.width, tieLayerSize.height);
-                // Then get a graphics context for the new layer
-                //CGContextRef tieContextRef = CGLayerGetContext(tieLayer);
-                // Now hold onto the state because we're about to majorly mess things up.
-                //CGContextSaveGState(tieContextRef);
-                // Move the origin to the center so that we rotate and scale around this point
-                //CGContextTranslateCTM(tieContextRef, tieLayerSize.width/2, tieLayerSize.height/2);
-                // After re-centering, apply the same transform that the tie had.
-                //CGContextConcatCTM(tieContextRef, ctm);
-                // Draw the tie image in the tie layer
-                //CGContextDrawImage(tieContextRef, [[ctrl previewLayer] bounds], base.CGImage);
-                // Now restore the layer state (might not be needed)
-                //CGContextRestoreGState(tieContextRef);
-                // NOW MOVING BACK TO MAIN CONTEXT
-                
-                // Now figure out a rectangle to draw the tieLayer within and draw it.
-                //NSInteger scaleFactor = base.size.width / tieLayerSize.width;
-                //NSInteger offset = (base.size.height - tieLayerSize.height) - 2;
-                //CGRect tieRect = CGRectMake(0, offset, scaleFactor*tieLayerSize.width, scaleFactor*tieLayerSize.height);
-                //CGContextDrawLayerInRect(refContext, tieRect, tieLayer);
-        
-                //** TIE DRAWING CODE...
-                // Now hold onto the state because we're about to majorly mess things up.
+                // TIE DRAWING CODE
+                // ================
+                // Now scale the translation to the new size and update the completed transformation for the tie.
+                CGPoint scaledTranslation = CGPointMake(translation.x * scale, translation.y * scale);
+                // Get ready to build the final movement (I feel like Beethoven right now)
+                CGAffineTransform totalTransform;
+                // setup a transform based on the scaled translation (we adjusted this from screen-space to image-space)
+                totalTransform = CGAffineTransformMakeTranslation(scaledTranslation.x, scaledTranslation.y);
+                // Merge the scale/rotation transformation with the translation tranformation
+                totalTransform = CGAffineTransformConcat(ctm, totalTransform);
+                // Now hold onto the state because we're about to majorly mess things up (with that transform)
                 CGContextSaveGState(refContext);
                 // Move the origin to the center so that we rotate and scale around this point
                 CGContextTranslateCTM(refContext, finalSize.width/2, finalSize.height/2);
-                // After re-centering, apply the same transform that the tie had.
+                // After re-centering, apply the same transform that the tie had (adjusted for our image)
                 CGContextConcatCTM(refContext, totalTransform);
-                // Draw with an adjusted center point.
+                // Draw the tie with an adjusted center point and the transforms applied (args: x,y,width,height)
                 [tie drawInRect:CGRectMake(-finalSize.width / 2, -finalSize.height / 2,
                                            finalSize.width, finalSize.height)];
-                // OK - restore the regular transform matrix
+                // OK - restore the regular transform matrix (extend arm, drop mic, walk out)
                 CGContextRestoreGState(refContext);
-                //*/
+                
                 
                 // Add a Tie Rack brand in the lower left.
                 UIImage *branding = [UIImage imageNamed:@"TieRack-imageBrand"];
@@ -137,10 +140,10 @@
                 UIImage *final = UIGraphicsGetImageFromCurrentImageContext();
                 UIGraphicsEndImageContext();
                 
-                NSLog(@"Have image... writing to Camera Roll...");
+                //NSLog(@"Have image... writing to Camera Roll...");
                 UIImageWriteToSavedPhotosAlbum(final, nil, nil, nil);
                 
-                NSLog(@"Removing the stillImageOutput");
+                //NSLog(@"Removing the stillImageOutput");
                 [session removeOutput:stillImageOut];
             }];
         }
